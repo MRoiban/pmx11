8d97336b23d78b11fff373e2566c18e33899423c
MRoiban <roiban.marius.alexandru@gmail.com>
2024-09-04 12:02:58 +0200

    Update program.asm and program.rom

--- program.asm
+++ program.asm
@@ -1,8 +1,5 @@
-CALL @draw_alex
+POT #10
+POT 0x12
+STR
+MOV 0x12, R2
 HALT
-LABEL @draw_alex
-WCHR A, #5, #5, #2, 0xf00
-WCHR L, #11, #5, #2, 0xf00
-WCHR E, #16, #5, #2, 0xf00
-WCHR X, #21, #5, #2, 0xf00
-JMP
