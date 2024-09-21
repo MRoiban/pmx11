b31473adefa7894416eab420d569050ae32a8830
MRoiban <roiban.marius.alexandru@gmail.com>
2024-09-21 12:07:37 +0200

    Add new code and update existing code

--- program.asm
+++ program.asm
@@ -1,5 +1,20 @@
-POT #10
-POT 0x12
-STR
-MOV 0x12, R2
+//! Turn on the screen
+POT 1
+DVW 0x12
+
+//! Draw something 
+WCHR M, #9, #5, #4, 0xf00
+WCHR A, #15, #5, #4, 0xf00
+WCHR T, #21, #5, #4, 0xf00
+POT 0
+DVW 0x12
+WCHR E, #27, #5, #4, 0xf00
+WCHR U, #33, #5, #4, 0xf00
+WCHR S, #40, #5, #4, 0xf00
+WCHR Z, #46, #5, #4, 0xf00
+POT 1
+DVW 0x12
+
+
+
 HALT
