d5e5df102174f2a3184cb5fb72fa0c76c53ab05b
MRoiban <roiban.marius.alexandru+github@gmail.com>
2024-12-21 01:37:38 +0100

    documentation(program.asm): clarify screen update comments for better understanding

--- program.asm
+++ program.asm
@@ -1,4 +1,4 @@
-//! Turn on the screen
+//! Turn on the screen updates
 POT 1
 DVW 0x12
 
@@ -6,14 +6,17 @@ DVW 0x12
 WCHR M, #9, #5, #4, 0xf00
 WCHR A, #15, #5, #4, 0xf00
 WCHR T, #21, #5, #4, 0xf00
+// Turn off screen updates
 POT 0
 DVW 0x12
 WCHR E, #27, #5, #4, 0xf00
 WCHR U, #33, #5, #4, 0xf00
 WCHR S, #40, #5, #4, 0xf00
 WCHR Z, #46, #5, #4, 0xf00
+
 POT 1
 DVW 0x12
+
 
 
 
