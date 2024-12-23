9c3289c692e90aae1ffa31f7017d79b6d4fed598
MRoiban <roiban.marius.alexandru+github@gmail.com>
2024-12-23 10:22:07 +0200

    refactor(program.asm): replace immediate values with register loads for improved flexibility

--- program.asm
+++ program.asm
@@ -1,23 +1,15 @@
-//! Turn on the screen updates
+// Turn on the screen updates
 POT 1
 DVW 0x12
 
-//! Draw something 
-WCHR M, #9, #5, #4, 0xf00
-WCHR A, #15, #5, #4, 0xf00
-WCHR T, #21, #5, #4, 0xf00
-// Turn off screen updates
-POT 0
-DVW 0x12
-WCHR E, #27, #5, #4, 0xf00
-WCHR U, #33, #5, #4, 0xf00
-WCHR S, #40, #5, #4, 0xf00
-WCHR Z, #46, #5, #4, 0xf00
+LOAD R1, #9
+LOAD R2, #5
+LOAD R3, #4
+LOAD R4, 0xfff
 
-POT 1
-DVW 0x12
-
-
+// Draw something 
+WCHR M, R1, R2, R3, R4
+//WCHR M, #9, #5, #4, R4
 
 
 HALT
