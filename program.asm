171e3a362e8a66362a9cb31f6daaeeb8011cd26a
MRoiban <roiban.marius.alexandru+github@gmail.com>
2024-12-23 21:17:50 +0200

    refactor(program.asm, program.rom): update drawing parameters and add new data entry

--- program.asm
+++ program.asm
@@ -2,12 +2,18 @@
 POT 1
 DVW 0x12
 
+POT 1
+DVW 0x20
+
+
 LOAD R1, #9
 LOAD R2, #5
 LOAD R3, #4
 LOAD R4, 0xfff
 
-// Draw something 
-WCHR 1, R1, R2, R3, R4
 
+
+// Draw something 
+WCHR 0, R1, R2, R3, R4
 HALT
+
