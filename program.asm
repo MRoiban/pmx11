68edd72780f950f6e64fe8dfafd8fd060ecf4967
MRoiban <roiban.marius.alexandru+github@gmail.com>
2024-12-25 20:30:44 +0200

    chore(test)

--- program.asm
+++ program.asm
@@ -1,11 +1,6 @@
-// NOT WORKING
-// VAR X, #10              
-// MOV X, R1         
-     
-; Turn on the screen updates
-POT 1
-DVW 0x12 ; screen drawing from memory
-
-; Draw something 
-WCHR M, #9, #5, #4, 0xfff
+POT #10
+POT #2
+DIV ; testing new division opcode
+POT #10
+MUL ; testing new multiplication opcode
 HALT
