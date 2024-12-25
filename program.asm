20d334b8744ee08c5c173544460db861ad37197a
MRoiban <roiban.marius.alexandru+github@gmail.com>
2024-12-25 16:21:03 +0200

    refactor(program.asm, program.rom): simplify screen update logic and reorganize ROM data

--- program.asm
+++ program.asm
@@ -1,19 +1,11 @@
-// Turn on the screen updates
+// NOT WORKING
+// VAR X, #10              
+// MOV X, R1         
+     
+; Turn on the screen updates
 POT 1
-DVW 0x12
+DVW 0x12 ; screen drawing from memory
 
-POT 1
-DVW 0x20
-
-
-LOAD R1, #9
-LOAD R2, #5
-LOAD R3, #4
-LOAD R4, 0xfff
-
-
-
-// Draw something 
-WCHR 0, R1, R2, R3, R4
+; Draw something 
+WCHR M, #9, #5, #4, 0xfff
 HALT
-
