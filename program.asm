b3fb536773762005f6f3093399aaa265016f0082
MRoiban <roiban.marius.alexandru+github@gmail.com>
2024-12-25 21:45:40 +0200

    test

--- program.asm
+++ program.asm
@@ -1,9 +1,13 @@
-POT #10
-POT #2
-DIV ; testing new division opcode
-POT #10
-MUL ; testing new multiplication opcode
-SQRT
-POT #3
-POW
-HALT
+    POT #10
+    POT #2
+    DIV     ; testing new division opcode
+    POT #10
+    MUL     ; testing new multiplication opcode
+    POT #3
+    POW
+    POT #1
+    DVW 0x10 ; Turn on the screen
+    POT #1
+    DVW 0x12 ; Draw char from mem
+    WCHR M, #9, #5, #4, 0xfff 
+    HALT
