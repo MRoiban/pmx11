4f0f3d74cb7c40d3d313e4759fb6e114b1ed5958
MRoiban <roiban.marius.alexandru+github@gmail.com>
2024-12-26 10:43:34 +0200

    feat(devices): initialize console structure with default values

--- src/devices/console.c
+++ src/devices/console.c
@@ -1,1 +1,13 @@
 #include "./console.h"
+
+PMXConsole pmx_console = {
+    .cursor_x = 0,
+    .cursor_y = 0,
+    .height = 0,
+    .width = 0,
+    .scale = 1,
+};
+
+void init_console() {
+    
+}
--- src/devices/console.h
+++ src/devices/console.h
@@ -1,16 +1,19 @@
 #include "../pmx.h"
 
-#ifndef CONSOLE
-#define CONSOLE
+#ifndef PMX_CONSOLE
+#define PMX_CONSOLE
 
-typedef struct console
+typedef struct PMXConsole
 {
     int width, height, scale, cursor_x, cursor_y;
     char* stdin;
     char* stdout;
     char* stderr;
     char* history;
-};
+} PMXConsole;
 
+extern PMXConsole pmx_console;
+
+void init_console();
 
 #endif 
