564c369836f6118ff58cd36b8c5ced8be73fa7b8
MRoiban <roiban.marius.alexandru+github@gmail.com>
2024-12-26 10:37:08 +0200

    style(devices): remove unnecessary comments in mouse module

--- src/devices/mouse.c
+++ src/devices/mouse.c
@@ -16,15 +16,15 @@ void mouse_update() {
     SDL_Event event;
     while (SDL_PollEvent(&event)) {
         if (event.type == SDL_MOUSEMOTION) {
-            pmx_mouse.x = event.motion.x; // Relative x-motion
-            pmx_mouse.y = event.motion.y; // Relative y-motion
+            pmx_mouse.x = event.motion.x; 
+            pmx_mouse.y = event.motion.y; 
         } else if (event.type == SDL_MOUSEBUTTONDOWN || event.type == SDL_MOUSEBUTTONUP) {
             int button_flag = (event.button.button == SDL_BUTTON_LEFT) ? 0x1 :
                               (event.button.button == SDL_BUTTON_RIGHT) ? 0x2 : 0x4;
             if (event.type == SDL_MOUSEBUTTONDOWN) {
-                pmx_mouse.buttons |= button_flag; // Set button state
+                pmx_mouse.buttons |= button_flag; 
             } else {
-                pmx_mouse.buttons &= ~button_flag; // Clear button state
+                pmx_mouse.buttons &= ~button_flag; 
             }
         }
     }
@@ -32,8 +32,6 @@ void mouse_update() {
 
 void
 mouse_deo(PMX *pmx, Uint8 addr) {
-    // printf("dev: %d\n", pmx->dev[addr]);
-    // printf("deo addr: %d\n", pmx->dev[addr]);
     switch (addr) {
     case 0x20:
         pmx->dev[0x25] = pmx_mouse.x;
