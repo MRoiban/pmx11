e78075c7e108366e1b8ee8803e168776b4d1ac1b
MRoiban <roiban.marius.alexandru+github@gmail.com>
2024-12-27 09:41:31 +0200

    refactor(asm): simplify program flow with loop structure

--- program.asm
+++ program.asm
@@ -1,16 +1,13 @@
-
-POT #1
-DVW 0x10
-POT #1
-DVW 0x12
-
 // Draw something 
-WCHR M, #9, #5, #4, 0xfff
-WCHR A, #15, #5, #4, 0xfff
-WCHR T, #21, #5, #4, 0xfff
-WCHR E, #27, #5, #4, 0xfff
-WCHR U, #33, #5, #4, 0xfff
-WCHR S, #40, #5, #4, 0xfff
-WCHR Z, #46, #5, #4, 0xfff
+; WCHR M, #9, #5, #4, 0xfff
+; WCHR A, #15, #5, #4, 0xfff
+; WCHR T, #21, #5, #4, 0xfff
+; WCHR E, #27, #5, #4, 0xfff
+; WCHR U, #33, #5, #4, 0xfff
+; WCHR S, #40, #5, #4, 0xfff
+; WCHR Z, #46, #5, #4, 0xfff
+
+LABEL @loop
+POT @loop
 
 HALT
