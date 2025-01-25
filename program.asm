b1dd10f5ba545c64d37d9173940bdc90f03f8d96
MRoiban <roiban.marius.alexandru+github@gmail.com>
2025-01-25 18:43:42 +0100

    refactor(program.asm): simplify and clean up drawing and memory operations

--- program.asm
+++ program.asm
@@ -1,32 +1,20 @@
-// Draw something -> to WSTR
-; WCHR M, #9, #5, #4, 0xfff
-; WCHR A, #15, #5, #4, 0xfff
-; WCHR T, #21, #5, #4, 0xfff
-; WCHR E, #27, #5, #4, 0xfff
-; WCHR U, #33, #5, #4, 0xfff
-; WCHR S, #40, #5, #4, 0xfff
-; WCHR Z, #46, #5, #4, 0xfff
+POT 0x41
+POT 0x41
+STR
+POT 0x42
+POT 0x42
+STR
 
-;POT #1
-;LABEL loop 
-;INC
-;JMP $loop
+POT 0x41
+DVW 0x36
+POT 0x02
+DVW 0x37
+POT #1
+DVW 0x30
 
-POT #10
-LOAD R1, #10
-MOV R1, R2
-
+//LOAD R1, #3
 POT #1
-DVW 0x20
-POT #1
-DVW 0x10
-POT #1
-DVW 0x12
-POT #1
-DVW 0x13
-
-WSTR BILAL12, #9, #5, #4, 0xfff
-POT #1
-
+POT #2
+ADD
 
 HALT
