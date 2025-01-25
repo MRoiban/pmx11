9bd96bcb12c8dac98a6b459beff572498517eaf9
MRoiban <roiban.marius.alexandru+github@gmail.com>
2025-01-25 11:57:25 +0100

    refactor(program.asm): replace drawing routines with WSTR and update program flow

--- program.asm
+++ program.asm
@@ -1,4 +1,4 @@
-// Draw something 
+// Draw something -> to WSTR
 ; WCHR M, #9, #5, #4, 0xfff
 ; WCHR A, #15, #5, #4, 0xfff
 ; WCHR T, #21, #5, #4, 0xfff
@@ -7,7 +7,24 @@
 ; WCHR S, #40, #5, #4, 0xfff
 ; WCHR Z, #46, #5, #4, 0xfff
 
-LABEL @loop
-POT @loop
+;POT #1
+;LABEL loop 
+;INC
+;JMP $loop
+
+POT #10
+LOAD R1, #10
+MOV R1, R2
+
+POT #1
+DVW 0x20
+POT #1
+DVW 0x10
+POT #1
+DVW 0x12
+POT #1
+DVW 0x13
+
+WSTR BILAL12, #9, #5, #4, 0xfff
 
 HALT
