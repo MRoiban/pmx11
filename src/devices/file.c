6941f8f9ac71e9228f7857878f0bc54f9958310d
MRoiban <roiban.marius.alexandru+github@gmail.com>
2025-01-25 18:43:46 +0100

    feat(file): implement file operations and integrate with PMXFile struct refactor(file): update file_deo to use PEEK/PEEK2 macros for memory access refactor(file): add file_reset, file_init, file_write, file_read, and file_delete functions style(file): reorder includes and adjust code formatting for consistency

--- src/devices/file.c
+++ src/devices/file.c
@@ -1,59 +1,116 @@
-#include <stdio.h>
 #include <dirent.h>
 #include <errno.h>
 #include <limits.h>
-#include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
 #include <unistd.h>
 
 #include "file.h"
+
+#define MAX_FILES 1024
 
 typedef struct PMXFile {
     DIR *dir;
     FILE *f;
     char filename[1024];
     struct dirent *de;
+
     enum state {
         IDLE,
         FILE_READ,
         FILE_WRITE,
     } state;
+
     int outside_sandbox;
 } PMXFile;
 
-void
-file_reset() {}
+PMXFile pmxfile[MAX_FILES];
 
 void
-file_init() {}
+file_reset(PMXFile *file) {
+    if (file->f != NULL) {
+        fclose(file->f);
+        file->f = NULL;
+    }
+    file->state = IDLE;
+    file->outside_sandbox = 0;
+    memset(file->filename, 0, sizeof(file->filename));
+}
 
-void
-file_write() {}
+static Uint16
+file_init(PMXFile *file, char *filename, int max_len) {
+    char *p = file->filename;
+    int len = sizeof(p);
+    if (len > max_len)
+        len = max_len;
+    while (len) {
+        *p++ = *filename++;
+        len--;
+    }
+    return 0;
+}
 
-void
-file_read() {}
+static Uint16
+file_write(PMXFile *file, void *src, int len, Uint8 flags) {
+    if (file->state != FILE_WRITE) {
+        if ((file->f = fopen(file->filename, (flags & 0x01) ? "ab" : "wb")) !=
+            NULL) {
+            file->state = FILE_WRITE;
+        }
+    }
+    if (file->state == FILE_WRITE)
+        fwrite(src, 1, len, file->f);
 
-void
-file_delete() {}
+    return 0;
+}
+
+static Uint16
+file_read(PMXFile *file, void *dest, int len) {
+    if (file->state != FILE_READ) {
+        if ((file->f = fopen(file->filename, "rb")) != NULL) {
+            file->state = FILE_READ;
+        }
+    }
+    if (file->state == FILE_READ)
+        fread(dest, 1, len, file->f);
+    return 0;
+}
+
+static Uint16
+file_delete(PMXFile *file) {
+    file_reset(file);
+    return unlink(file->filename);
+}
 
 void
 file_deo(PMX *pmx, Uint8 addr) {
     switch (addr) {
-    case 0x30:
-        file_init();
+    case 0x30: {
+        int addr = PEEK2(pmx, 0x36);
+        int len = PEEK2(pmx, 0x37);
+        char filename[len];
+        for (int i = 0; i < len; i++) {
+            filename[i] = PEEK(pmx, addr + i);
+        }
+        file_init(&pmxfile[0], filename, len);
+        printf("filename: %s\n", pmxfile[0].filename);
+        POKE2(pmx, 0x30, 0);
         break;
+    }
     case 0x31:
-        file_reset();
+        file_reset(&pmxfile[0]);
         break;
     case 0x32:
-        file_read();
+        file_read(&pmxfile[0], &pmx->memory[0x100], PEEK2(pmx, 0x37));
         break;
     case 0x33:
-        file_write();
+        file_write(&pmxfile[0], &pmx->memory[0x100], PEEK2(pmx, 0x37),
+                   PEEK2(pmx, 0x38));
         break;
     case 0x34:
-        file_delete();
+        file_delete(&pmxfile[0]);
         break;
     default:
         break;
